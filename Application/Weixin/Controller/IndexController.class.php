34eacaebcc8bb3c4717b939d90d0acde29c6be99
wslshanlin <wslshanlin@sina.com>
2016-09-27 11:26:53 +0800

    用户注册账号 | IndexController->userData_post_json()

--- Application/Weixin/Controller/IndexController.class.php
+++ Application/Weixin/Controller/IndexController.class.php
@@ -1,14 +1,46 @@
 <?php
 namespace Weixin\Controller;
+
 use Think\Controller;
 use Think\Controller\RestController;
-class IndexController extends RestController {
-    public function index(){
-//        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+
+class IndexController extends RestController
+{
+
+    protected $allowMethod = ['get', 'post', 'put', 'delete'];
+    protected $allowType = ['html', 'json'];
+    protected $defaultMethod = 'get';
+    protected $defaultType = 'html';
+
+
+//    判断用户名是否存在
+    public function userExist_get_json()
+    {
+        // 允许跨域访问
+        header("Access-Control-Allow-Origin: *");
+
+        $this->response([], 'json');
+    }
+
+//    增加一条用户信息
+    public function userData_post_json()
+    {
+        // 允许跨域访问
+        header("Access-Control-Allow-Origin: *");
+
+        $data['phone'] = I('post.phone');
+        $data['password'] = md5(I('post.password2'));
+        $data['idcard'] = I('post.idcard');
+        $data['regtime'] = date('Y-m-d');
+
+        $User = M('user');
+        $User->data($data)->add();
+        $this->response($data, 'json');
+
 
     }
 
-
+//    修改用户信息
 
 
 //    ！微信接入类 WeiXin TP 自带
