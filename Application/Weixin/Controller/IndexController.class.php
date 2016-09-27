3226a6fd14e48cdb4bb00494c9f66986de0e5be9
wslshanlin <wslshanlin@sina.com>
2016-09-27 15:55:01 +0800

    Wexin 模块 | 用户注册 | captcha_button() | form_check() | form_submit() | userExist_get_json()

--- Application/Weixin/Controller/IndexController.class.php
+++ Application/Weixin/Controller/IndexController.class.php
@@ -16,28 +16,34 @@ class IndexController extends RestController
 //    判断用户名是否存在
     public function userExist_get_json()
     {
-        // 允许跨域访问
+//         允许跨域访问
         header("Access-Control-Allow-Origin: *");
 
-        $this->response([], 'json');
+        $request['phone'] = I('phone');
+        $User = M('user');
+        $data = $User->where($request)->select();
+        if (count($data) > 0) {
+            $this->response(['isExist' => 1], 'json');
+        } else {
+            $this->response(['isExist' => 0], 'json');
+        }
     }
 
 //    增加一条用户信息
     public function userData_post_json()
     {
-        // 允许跨域访问
+//         允许跨域访问
         header("Access-Control-Allow-Origin: *");
 
+//        将表单数据插入数据库
+//        todo 再次验证手机号是否重复
         $data['phone'] = I('post.phone');
         $data['password'] = md5(I('post.password2'));
         $data['idcard'] = I('post.idcard');
         $data['regtime'] = date('Y-m-d');
-
         $User = M('user');
         $User->data($data)->add();
         $this->response($data, 'json');
-
-
     }
 
 //    修改用户信息
