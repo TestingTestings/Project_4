47cd7505a517a33bfc14bf85acda5e52381e2bca
huiyaoren <wslshanlin@sina.com>
2016-09-28 22:25:12 +0800

    登录 ajax | 验证码无效 | 身份证号字段无法写入

--- Application/Weixin/Controller/IndexController.class.php
+++ Application/Weixin/Controller/IndexController.class.php
@@ -21,6 +21,7 @@ class IndexController extends RestController
         $request['phone'] = I('phone');
         $User = M('user');
         $data = $User->where($request)->select();
+
         if (count($data) > 0) {
             $this->response(['isExist' => 1], 'json');
         } else {
@@ -39,27 +40,58 @@ class IndexController extends RestController
         $data['password'] = md5(I('post.password2'));
         $data['idcard'] = I('post.idcard');
         $data['regtime'] = date('Y-m-d');
+
         $User = M('user');
         $User->data($data)->add();
         $this->response($data, 'json');
     }
 
-//    判断用户登录数据
-    public function userLogin_get_json()
-    {
-        header("Access-Control-Allow-Origin: *"); // 允许跨域访问
-    }
-
 //    生成验证码图片
     public function captcha_get_png()
     {
+        header("Access-Control-Allow-Origin: *"); // 允许跨域访问
+
         $config = [
             'fontSize' => 50,
             'length' => 4,
             'useNoise' => true
         ];
+
         $Verify = new \Think\Verify($config);
         $Verify->entry();
+    }
+
+//    判断用户登录数据
+    public function userLogin_put_json()
+    {
+        header("Access-Control-Allow-Origin: *"); // 允许跨域访问
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE"); // 允许的跨域请求方式
+//
+        $request['phone'] = I('phone');
+        $request['password'] = md5(I('password'));
+
+        $data['phone'] = $request['phone'];
+        $data['password'] = M('user')->where($data)->getField('password');
+
+        $response['password2'] = md5(I('password'));
+        $response['password'] = $data['password'];
+        $response['captcha'] = I('captcha');
+        $response['verify'] = $this->check_verify(I('captcha'));
+
+        if (false) {
+            return ;
+        }
+        if ($request['password'] == $data['password']) {
+            $response['isConfirm'] = 1;
+            $this->response($response, 'json');
+        }
+    }
+
+
+    private function check_verify($code, $id = '')
+    {
+        $verify = new \Think\Verify();
+        return $verify->check($code, $id);
     }
 
 
@@ -77,4 +109,7 @@ class IndexController extends RestController
 //    个人信息
 //    *新闻读取
 //    *法规查询
+
+//    验证码校验
+
 }
