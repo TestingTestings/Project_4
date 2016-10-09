3a867cd922c1ca86295d9e38e468440c6e0a88df
huiyaoren <wslshanlin@sina.com>
2016-10-09 16:35:54 +0800

    doc | 修改触发器

--- doc/sql/trigger.sql
+++ doc/sql/trigger.sql
@@ -2,7 +2,7 @@ DROP TRIGGER tg1;
 CREATE TRIGGER tg1 AFTER INSERT ON t_casehandle
 FOR EACH ROW
   BEGIN
-    UPDATE t_case
-    SET state = "申诉"
-    WHERE id = new.case_id;
+    IF (new.state = '申诉') then
+      UPDATE t_case SET state = '申诉'  WHERE id = new.case_id;
+    END IF;
   END;
