eee954ea56368adc65390e85d43c8fc20af0de04
linzi <295568253>
2016-10-17 11:26:10 +0800

    app协查后台

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -264,6 +264,12 @@ class IndexController extends Controller {
         $one_case=$detail->where("id='{$case_id}'")->save();//新建案件模型
         $this->search("case");
     }
+    //协查通报
+    public function criminal(){
+        $crim=M('criminal');
+        $res=$crim->where("state='在逃'")->select();
+        echo json_encode($res);
+    }
 }
 
 //搜索函数构造索引数组
